246ffcf885ff7136d1db672cfc9bd9f1f1b97b20
TanushreeNayak <134424763+TanushreeNayak@users.noreply.github.com>
2023-05-23 22:17:10 +0530

    Create Fibo_Recurs.py

---
+++ Fibo_Recurs.py
@@ -0,0 +1,14 @@
+def fiboSeries(i):
+    if i <= 1:
+        return i
+    else:
+        return (fiboSeries(i - 1) + (fiboSeries(i - 2))
+
+num = int(input("Enter any Number: "))
+
+if num <= 0:
+    print("Number is negative")
+else:
+    print("Fibonacci series",end = " ")
+for i in range(num):
+    print(fiboSeries(i), end="")
